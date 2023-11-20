b6353e056c7057204e7461ef1f55dd491309529e
Jonas Ploth <151474840+jonasploth@users.noreply.github.com>
2023-11-20 06:07:54 +0100

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,6 @@
 
 
 function main() {
-  
   // Configuration Section
   var spreadsheetUrl = 'YOUR_SPREADSHEET_URL'; // Replace with the URL of your Google Sheet
   var campaignIds = ['CAMPAIGN_ID_1', 'CAMPAIGN_ID_2', 'CAMPAIGN_ID_3']; // Add campaign IDs 
@@ -17,9 +16,8 @@ function main() {
   var emailSubject = 'Google Ads Script Report Completed'; // Customize the email subject
   var emailBody = 'The report for your Google Ads campaigns has been completed.\n\n' +
                   'You can view the report at: ' + spreadsheetUrl; // Customize the email body
-  // Configuration Section
-  
-  // Convert cost threshold to micro-units
+
+  // Convert cost
   var costThreshold = costThresholdInEuros * 1000000;
   var excludedKeywordsInfo = []; // To keep track of excluded keywords
 
@@ -28,10 +26,10 @@ function main() {
   var sheetName = "Scan on " + today;
   var sheet = spreadsheet.getSheetByName(sheetName);
 
-  // Create a new sheet
+  // Create a new sheet if not existing
   if (!sheet) {
     sheet = spreadsheet.insertSheet(sheetName);
-    sheet.appendRow(['Campaign Name', 'Search Term', 'Cost', 'Conversions']);
+    sheet.appendRow(['Campaign Name', 'Ad Group Name', 'Search Term', 'Cost', 'Conversions']);
   }
 
   var endDate = new Date();
@@ -43,84 +41,69 @@ function main() {
 
   for (var i = 0; i < campaignIds.length; i++) {
     var campaignId = campaignIds[i];
-    var campaignName = getCampaignNameById(campaignId); // get campaignName
+    var campaign = AdsApp.campaigns().withIds([campaignId]).get().next();
+    var campaignName = campaign.getName();
+    var adGroups = campaign.adGroups().get();
 
-    var query = "SELECT CampaignId, Query, Cost, Conversions " +
-                "FROM SEARCH_QUERY_PERFORMANCE_REPORT " +
-                "WHERE CampaignId = '" + campaignId + "' " +
-                "AND Conversions = 0 " +
-                "AND Cost > " + costThreshold + " " +
-                "DURING " + formattedStartDate + "," + formattedEndDate;
+    while (adGroups.hasNext()) {
+      var adGroup = adGroups.next();
+      var adGroupId = adGroup.getId();
+      var adGroupName = adGroup.getName();
 
-    var report = AdsApp.report(query);
-    var rows = report.rows();
-    var exclusionList = [];
+      var query = "SELECT AdGroupId, Query, Cost, Conversions " +
+                  "FROM SEARCH_QUERY_PERFORMANCE_REPORT " +
+                  "WHERE AdGroupId = '" + adGroupId + "' " +
+                  "AND Conversions = 0 " +
+                  "AND Cost > " + costThreshold + " " +
+                  "DURING " + formattedStartDate + "," + formattedEndDate;
 
-    while (rows.hasNext()) {
-      var row = rows.next();
-      var searchTerm = row['Query'];
-      var cost = parseFloat(row['Cost']); 
-      var conversions = row['Conversions'];
-      sheet.appendRow([campaignName, searchTerm, cost, conversions]);
+      var report = AdsApp.report(query);
+      var rows = report.rows();
+      var exclusionList = [];
 
-      if (createAndExclude === 'YES') {
-        exclusionList.push(searchTerm);
+      while (rows.hasNext()) {
+        var row = rows.next();
+        var searchTerm = row['Query'];
+        var cost = parseFloat(row['Cost']);
+        var conversions = row['Conversions'];
+        sheet.appendRow([campaignName, adGroupName, searchTerm, cost, conversions]);
+
+        if (createAndExclude === 'YES') {
+          exclusionList.push(searchTerm);
+        }
       }
-    }
 
-    if (createAndExclude === 'YES' && exclusionList.length > 0) {
-      var keywordsExcluded = addKeywordsToExclusionList(campaignId, exclusionList);
-      if (keywordsExcluded > 0) {
-        excludedKeywordsInfo.push({ campaignId: campaignId, count: keywordsExcluded });
+      if (createAndExclude === 'YES' && exclusionList.length > 0) {
+        var keywordsExcluded = addKeywordsToExclusionList(adGroupId, exclusionList);
+        if (keywordsExcluded > 0) {
+          excludedKeywordsInfo.push({ adGroupName: adGroupName, count: keywordsExcluded });
+        }
       }
     }
   }
 
   // Append information about automatically excluded keywords to the email body
   if (excludedKeywordsInfo.length > 0) {
-    emailBody += '\n\nKeywords were automatically excluded in the following campaigns:';
+    emailBody += '\n\nKeywords were automatically excluded in the following ad groups:';
     for (var i = 0; i < excludedKeywordsInfo.length; i++) {
       var info = excludedKeywordsInfo[i];
-      emailBody += '\nCampaign ID ' + info.campaignId + ': ' + info.count + ' keywords';
+      emailBody += '\nAd Group ' + info.adGroupName + ': ' + info.count + ' keywords';
     }
   }
 
   MailApp.sendEmail(notificationEmail, emailSubject, emailBody);
 }
 
-function getCampaignNameById(campaignId) {
-  var campaignIterator = AdsApp.campaigns().withIds([campaignId]).get();
-  if (campaignIterator.hasNext()) {
-    return campaignIterator.next().getName();
-  }
-  return 'Unknown Campaign'; // Default name if the campaign is not found
-}
-
-function addKeywordsToExclusionList(campaignId, keywords) {
-  var campaign = AdsApp.campaigns().withIds([campaignId]).get().next();
-  var negativeKeywordList;
-  var keywordListIterator = AdsApp.negativeKeywordLists()
-                                  .withCondition('Name = "Exclusion List for Campaign ' + campaignId + '"')
-                                  .get();
-
-  // Check if the exclusion list already exists
-  if (keywordListIterator.hasNext()) {
-    negativeKeywordList = keywordListIterator.next();
-  } else {
-    negativeKeywordList = AdsApp.newNegativeKeywordListBuilder()
-                                .withName('Exclusion List for Campaign ' + campaignId)
-                                .build()
-                                .getResult();
-  }
-
+function addKeywordsToExclusionList(adGroupId, keywords) {
+  var adGroup = AdsApp.adGroups().withIds([adGroupId]).get().next();
   var excludedCount = 0;
+
   for (var i = 0; i < keywords.length; i++) {
-    var exactMatchKeyword = '[' + keywords[i] + ']'; // Format as exact match
-    negativeKeywordList.addNegativeKeyword(exactMatchKeyword);
+    var exactMatchKeyword = '[' + keywords[i] + ']';
+    adGroup.createNegativeKeyword(exactMatchKeyword);
     excludedCount++;
   }
 
-  campaign.addNegativeKeywordList(negativeKeywordList);
   return excludedCount;
 }
 
