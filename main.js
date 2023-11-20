9e295f180f3563ed635cddc0c7f719acd33105dc
Jonas Ploth <151474840+jonasploth@users.noreply.github.com>
2023-11-20 04:39:27 +0100

    Update main.js

--- main.js
+++ main.js
@@ -14,18 +14,19 @@ function main() {
   var createAndExclude = 'NO'; // Set to 'YES' or 'NO'
   var notificationEmail = 'YOUR_EMAIL_ADDRESS'; // Set the email address for notifications
   var emailSubject = 'Google Ads Script Report Completed'; // Customize the email subject
-  var emailBody = 'The report for your Google Ads campaigns has been completed.\n\nYou can view the report at: ' + spreadsheetUrl; // Customize the email body
+  var emailBody = 'The report for your Google Ads campaigns has been completed.\n\n' +
+                  'You can view the report at: ' + spreadsheetUrl; // Customize the email body
+
   // Convert cost threshold to micro-units
   var costThreshold = costThresholdInEuros * 1000000;
-
-  // DO NOT TOUCH CODE BELOW HERE
+  var excludedKeywordsInfo = []; // To keep track of excluded keywords
 
   var spreadsheet = SpreadsheetApp.openByUrl(spreadsheetUrl);
   var today = Utilities.formatDate(new Date(), "GMT", "yyyyMMdd");
   var sheetName = "Scan on " + today;
   var sheet = spreadsheet.getSheetByName(sheetName);
 
-  // Create a new sheet if not existing for date
+  // Create a new sheet if it doesn't exist
   if (!sheet) {
     sheet = spreadsheet.insertSheet(sheetName);
     sheet.appendRow(['Campaign Name', 'Search Term', 'Cost', 'Conversions']);
@@ -40,7 +41,7 @@ function main() {
 
   for (var i = 0; i < campaignIds.length; i++) {
     var campaignId = campaignIds[i];
-    var campaignName = getCampaignNameById(campaignId); // get campaign name
+    var campaignName = getCampaignNameById(campaignId); // Fetch the campaign name
 
     var query = "SELECT CampaignId, Query, Cost, Conversions " +
                 "FROM SEARCH_QUERY_PERFORMANCE_REPORT " +
@@ -56,28 +57,37 @@ function main() {
     while (rows.hasNext()) {
       var row = rows.next();
       var searchTerm = row['Query'];
-      var cost = parseFloat(row['Cost']); // Convert cost from micro-units to Euros
+      var cost = parseFloat(row['Cost']) / 1000000; // Convert cost from micro-units to Euros
       var conversions = row['Conversions'];
       sheet.appendRow([campaignName, searchTerm, cost, conversions]);
-//check if needed to create exclusion list
+
       if (createAndExclude === 'YES') {
         exclusionList.push(searchTerm);
       }
     }
-//add search terms as keywords to exclude
+
     if (createAndExclude === 'YES' && exclusionList.length > 0) {
-      addKeywordsToExclusionList(campaignId, exclusionList); // add list to campaign
+      var keywordsExcluded = addKeywordsToExclusionList(campaignId, exclusionList);
+      if (keywordsExcluded > 0) {
+        excludedKeywordsInfo.push({ campaignId: campaignId, count: keywordsExcluded });
+      }
     }
   }
 
-  // Send email notification
+  // Append information about automatically excluded keywords to the email body
+  if (excludedKeywordsInfo.length > 0) {
+    emailBody += '\n\nKeywords were automatically excluded in the following campaigns:';
+    for (var i = 0; i < excludedKeywordsInfo.length; i++) {
+      var info = excludedKeywordsInfo[i];
+      emailBody += '\nCampaign ID ' + info.campaignId + ': ' + info.count + ' keywords';
+    }
+  }
+
   MailApp.sendEmail(notificationEmail, emailSubject, emailBody);
 }
 
 function getCampaignNameById(campaignId) {
-  var campaignIterator = AdsApp.campaigns()
-                          .withIds([campaignId])
-                          .get();
+  var campaignIterator = AdsApp.campaigns().withIds([campaignId]).get();
   if (campaignIterator.hasNext()) {
     return campaignIterator.next().getName();
   }
@@ -86,16 +96,28 @@ function getCampaignNameById(campaignId) {
 
 function addKeywordsToExclusionList(campaignId, keywords) {
   var campaign = AdsApp.campaigns().withIds([campaignId]).get().next();
-  var negativeKeywordList = AdsApp.newNegativeKeywordListBuilder()
-      .withName('Exclusion List for Campaign ' + campaignId)
-      .build()
-      .getResult();
+  var negativeKeywordList;
+  var keywordListIterator = AdsApp.negativeKeywordLists()
+                                  .withCondition('Name = "Exclusion List for Campaign ' + campaignId + '"')
+                                  .get();
 
+  // Check if the exclusion list already exists
+  if (keywordListIterator.hasNext()) {
+    negativeKeywordList = keywordListIterator.next();
+  } else {
+    negativeKeywordList = AdsApp.newNegativeKeywordListBuilder()
+                                .withName('Exclusion List for Campaign ' + campaignId)
+                                .build()
+                                .getResult();
+  }
+
+  var excludedCount = 0;
   for (var i = 0; i < keywords.length; i++) {
-    var exactMatchKeyword = '[' + keywords[i] + ']'; // Formatierung als Exact Match
+    var exactMatchKeyword = '[' + keywords[i] + ']'; // Format as exact match
     negativeKeywordList.addNegativeKeyword(exactMatchKeyword);
+    excludedCount++;
   }
 
   campaign.addNegativeKeywordList(negativeKeywordList);
+  return excludedCount;
 }
-
